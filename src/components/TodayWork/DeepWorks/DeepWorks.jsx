3a2f8d04f3504dfb7fd19f6de55a47a2db131fe9
Kaung Khant Mann Maung <itp26kkmm@gmail.com>
2024-08-26 13:58:25 +0200

    add SubTask on clicking MainTask button

--- src/components/TodayWork/DeepWorks/DeepWorks.jsx
+++ src/components/TodayWork/DeepWorks/DeepWorks.jsx
@@ -4,20 +4,35 @@ import styled from "styled-components";
 import Subject from "./Subject.jsx";
 import MainTask from "./MainTask.jsx";
 import SubTask from "./SubTask.jsx";
+import { SUB_TASKS_ADDABLE } from "../../../constants.js";
 
 function DeepWorks() {
-    const [subTasks, setSubtasks] = React.useState(["HomeWork(1)", "HomeWork(2)"])
-        
+    const [subTasks, setSubtasks] = React.useState([
+        "HomeWork(1)",
+        "HomeWork(2)",
+    ]);
+
     function addSubTask() {
-        
+        if (subTasks.length >= SUB_TASKS_ADDABLE) {
+            return;
+        }
+        setSubtasks([...subTasks, "New Sub Task"]);
     }
 
     return (
         <Wrapper>
             <Subject hasDesc={true}>Chemistry</Subject>
-            <MainTask addSubTask={addSubTask}>Memorize the periodic table</MainTask>
-            <SubTask subTasks={subTasks}/>
-
+            <MainTask addSubTask={addSubTask}>
+                Memorize the periodic table
+            </MainTask>
+            <SubTask subTasks={subTasks} />
+            {subTasks.length >= SUB_TASKS_ADDABLE && (
+                <SubTaskWarning>
+                    Whoa, that's a lot of tasks! You've maxed out your{" "}
+                    {SUB_TASKS_ADDABLE} sub-tasks. Time to check some off before
+                    adding more.{" "}
+                </SubTaskWarning>
+            )}
         </Wrapper>
     );
 }
@@ -30,8 +45,13 @@ const Wrapper = styled.section`
     max-width: 35%;
 `;
 
-
-
-
+const SubTaskWarning = styled.p`
+    align-self: center;
+    /* text-align: center; */
+    padding-left: 70px;
+    padding-right: 30px;
+    font-size: 0.8rem;
+    opacity: 0.8;
+`;
 
 export default DeepWorks;
