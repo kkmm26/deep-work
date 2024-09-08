88e601acf65cb5b551b6d1ddae576e627928d248
Kaung Khant Mann Maung <itp26kkmm@gmail.com>
2024-09-08 11:28:58 +0200

    update toast to show no more than 2

--- src/components/TodayWork/DeepWorks/ToastsProvider.jsx
+++ src/components/TodayWork/DeepWorks/ToastsProvider.jsx
@@ -2,16 +2,27 @@ import React from "react";
 
 export const ToastsContext = React.createContext();
 
-function ToastsProvider({children}) {
+function ToastsProvider({ children }) {
     const [toasts, setToasts] = React.useState([]);
     function createToast(taskName) {
-        setToasts([...toasts, { taskName, id: crypto.randomUUID() }]);
+        const newToast = {taskName, id:crypto.randomUUID()}
+
+        if (toasts.length >= 2) {
+            const lastToast = toasts[1]
+            setToasts([lastToast, newToast])
+            return
+        }
+        setToasts([...toasts, newToast]);
     }
     function destroyToast(idToDel) {
-        setToasts(toasts.filter(toast => toast.id !== idToDel))
+        setToasts(toasts.filter((toast) => toast.id !== idToDel));
     }
 
-    return <ToastsContext.Provider value={{toasts, createToast, destroyToast}}>{children}</ToastsContext.Provider>;
+    return (
+        <ToastsContext.Provider value={{ toasts, createToast, destroyToast }}>
+            {children}
+        </ToastsContext.Provider>
+    );
 }
 
 export default ToastsProvider;
